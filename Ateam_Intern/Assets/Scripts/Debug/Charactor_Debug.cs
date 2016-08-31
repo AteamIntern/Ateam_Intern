6643d54807b4d3f49d08944f438bb3de5c5aa1aa
manimani <manimani.0708@gmail.com>
2016-08-31 23:42:52 +0900

    キャラクターアイコンを押してる間はキャラクター情報の表示 敵キャラのHPゲージ変化

---
+++ Ateam_Intern/Assets/Scripts/Debug/Charactor_Debug.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Charactor_Debug : MonoBehaviour 
+{
+    public Sprite sprite { get; private set;}
+    public string text { get; private set;}
+
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        sprite = GetComponent<Image>().sprite;
+        text = "キャラクター情報";
+    }
+	
+}
---
+++ Ateam_Intern/Assets/Scripts/GameMain/CharactorIcon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.UI;
+
+public class CharactorIcon : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject charactorInfoPrefab;
+
+    [SerializeField]
+    private Charactor_Debug charactorInfo;
+    public Charactor_Debug _charactorInfo { get { return charactorInfo;} set { charactorInfo = value;}}
+
+    void Start()
+    {
+        GetComponent<Image>().sprite = charactorInfo.sprite;
+        transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = charactorInfo.text;
+
+
+        this.UpdateAsObservable()
+            .Select(_ => GetComponent<ButtonEx>().IsOn)
+            .DistinctUntilChanged()
+            .Subscribe(isOn => ChangeCharactorInfoView(isOn));
+    }
+
+    private void ChangeCharactorInfoView(bool isOn)
+    {
+        charactorInfoPrefab.SetActive(isOn);
+    }
+	
+}
